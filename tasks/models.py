3c47e0461f1a280cad708bfb306cc44d14700527
Middlewarer <mranarchy@mail.ru>
2025-04-28 16:51:26 +0100

    Task model and validation for deadline created

---
+++ tasks/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+def date_validation(value):
+    if value < timezone.now():
+        raise ValidationError('Нельзя назначить дату дедлайна в прошлое')
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateTimeField(validators=[date_validation])
+
